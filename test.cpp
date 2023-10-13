e921521b87e7d8c59be92f4f1b1e11a5bca5a469
muradil <2244755971@qq.com>
2023-10-14 01:13:03 +0800

    add updatetest2

--- test.cpp
+++ test.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main()
 {
-    cout<<"hello world,hello muradil,hello computer"<<endl;
-    cout<<"Creating a new branch is quick.";
+     cout<<"hello world,hello muradil,hello computer"<<endl;
+     cout<<"Creating a new branch is quick.";
 }
