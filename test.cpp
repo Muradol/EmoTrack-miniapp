f3bfbee0cbf441102c85ec44d1264759dc2ade81
1-cwc <2021694476@qq.com>
2023-10-14 01:11:59 +0800

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main()
 {
     cout<<"hello world,hello muradil,hello computer"<<endl;
-    cout<<"Creating a new branch is quick.";
+    cout<<"Creating a new branch is quick."; 
+    
+    cout<<"add test"<<endl;
 }
