a132d1be6045625b8071e55e8dead3c987d030d6
the star456 <1710355461@qq.com>
2023-10-16 02:44:21 +0000

    update test.cpp. 修改
    Signed-off-by: the star456 <1710355461@qq.com>

--- test.cpp
+++ test.cpp
@@ -6,5 +6,6 @@ int main()
     cout<<"hello world,hello muradil,hello computer"<<endl;
     cout<<"Creating a new branch is quick."; 
     cout<<"add test2"<<endl;
+    cout<<"HelloWorld"<<endl;
 
 }
